6ff62e873fd67a59a9c9024aeb7d7d30d498d544
KROSF <rodrigosanabria22@gmail.com>
2018-10-14 09:54:14 +0200

    modified double delta in test

--- practicas/src/test/java/com/krosf/pctr/p1/intDefinidaMonteCarloTest.java
+++ practicas/src/test/java/com/krosf/pctr/p1/intDefinidaMonteCarloTest.java
@@ -9,13 +9,13 @@ public class intDefinidaMonteCarloTest {
   @Test
   public void testSinx() {
     assertEquals(
-        0.45969769413, intDefinidaMonteCarlo.integralMonteCarlo(0, 1, 1000000, x -> Math.sin(x)), 0.0001);
+        0.45969769413, intDefinidaMonteCarlo.integralMonteCarlo(0, 1, 1000000, x -> Math.sin(x)), 0.001);
   }
 
   @Test
   public void testXcuadrado() {
     assertEquals(
-        0.33333333333, intDefinidaMonteCarlo.integralMonteCarlo(0, 1, 1000000, x -> Math.pow(x, 2)), 0.0003);
+        0.33333333333, intDefinidaMonteCarlo.integralMonteCarlo(0, 1, 1000000, x -> Math.pow(x, 2)), 0.003);
   }
 
   @Test
